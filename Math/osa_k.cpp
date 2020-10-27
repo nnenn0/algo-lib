7829378457034859eccd69acd5a1815875ebebc6
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2020-10-27 16:21:27 +0900

    add osa_k法

---
+++ Math/osa_k.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+std::vector<long long> sieve(long long n) {
+    std::vector<long long> res(n+1);
+    std::iota(res.begin(), res.end(), 0);
+    for (long long i = 2; i*i <= n; ++i) {
+        if (res[i] < i) continue;
+        for (long long j = i*i; j < n; j += i) {
+            if (res[j] == j) res[j] = i;
+        }
+    }
+    return res;
+}
+
+std::vector<long long> factor(long long n, const std::vector<long long>& min_factor) {
+    std::vector<long long> res;
+    while (n > 1) {
+        res.push_back(min_factor[n]);
+        n /= min_factor[n];
+    }
+    return res;
+}
+
+int main() {
+    int a = 10000;
+    auto min_factor = sieve(a);
+    std::cout << "check 100:" << std::endl;
+    for (auto&& res : factor(100, min_factor)) std::cout << res << std::endl;
+    std::cout << "check 777:" << std::endl;
+    for (auto&& res : factor(777, min_factor)) std::cout << res << std::endl;
+}
