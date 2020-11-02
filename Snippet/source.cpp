9003a218ad294006da606e15de6a872be663f37c
nnenn0 <geek.branch.info@gmail.com>
2020-11-02 17:51:37 +0900

    add  テンプレートmain

---
+++ Snippet/source.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int>>;
+using pint = pair<int, int>;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+const int IINF = 1LL << 29;
+const long long INF = 1LL << 60;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main() {
+    cout << setprecision(15) << fixed;
+    
+
+    return 0;
+}
