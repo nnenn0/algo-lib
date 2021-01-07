427d03cf2785a14e9a7927f276026286e9694635
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-01-07 22:21:20 +0900

    add nCk

---
+++ Math/nck.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr int MOD = 1000000007;
+
+vector<long long> fact, fact_inv, inv;
+/* 
+    二項係数のための前処理
+    init_nCk
+    計算量:O(n)
+*/
+void init_nCk(int SIZE) {
+    fact.resize(SIZE + 5);
+    fact_inv.resize(SIZE + 5);
+    inv.resize(SIZE + 5);
+    fact[0] = fact[1] = 1;
+    fact_inv[0] = fact_inv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < SIZE + 5; i++) {
+        fact[i] = fact[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
+    }
+}
+/* 
+    MODでの二項係数を求める
+    nCk O(1)
+    example: ABC145 Knight
+    https://atcoder.jp/contests/abc145/tasks/abc145_d
+*/
+
+long long nCk(int n, int k) {
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fact[n] * (fact_inv[k] * fact_inv[n - k] % MOD) % MOD;
+}
+
+long long solve(long long X, long long Y) {
+    init_nCk(3000000);
+    if ((X + Y) % 3 != 0) return 0;
+    long long n = (-X + Y*2) / 3;
+    long long m = (X*2 - Y) / 3;
+    return nCk(n+m, n);
+}
+
+int main() {
+    long long X, Y; cin >> X >> Y;
+    cout << solve(X, Y) << endl;
+    return 0;
+}
