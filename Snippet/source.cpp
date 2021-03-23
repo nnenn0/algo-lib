8559245466e56c51426b38fbcb625abfefcb1d45
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-03-24 00:02:39 +0900

    update User Snippets

--- Snippet/source.cpp
+++ Snippet/source.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define rrep(i,a,b) for(int i = a; i >= b; i--)
+#define fore(i,a) for(auto& i : a)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(),(x).end()
 using ll = long long;
-using Graph = vector<vector<int>>;
 using pint = pair<int, int>;
+using pll = pair<long long, long long>;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-const int IINF = 1LL << 29;
-const long long INF = 1LL << 60;
+const int IINF = 1000100100;
+const long long LINF = 1LL << 60;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
