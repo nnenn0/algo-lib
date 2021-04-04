581af58770df88dc0f0cd562223a8ec689e6686d
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-04-04 10:55:39 +0900

    add ダブリング

---
+++ Other/doubling.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+    ダブリング O(NlogK)
+    K回後の状態を求める。
+
+    example: ABC167D Teleporter
+    // https://atcoder.jp/contests/abc167/tasks/abc167_d
+*/
+
+int main() {
+    long long N, K; cin >> N >> K;
+    int log_k = 1;
+    while ((1LL << log_k) <= K) ++log_k;
+    vector<vector<int>> doubling(log_k, vector<int>(N));
+    for (int v = 0; v < N; ++v) cin >> doubling[0][v], --doubling[0][v];
+    for (int d = 0; d+1 < log_k; ++d) {
+        for (int v = 0; v < N; ++v) doubling[d+1][v] = doubling[d][doubling[d][v]];
+    }
+    int v = 0;
+    for (int d = 0; d < log_k; ++d) if (K & (1LL << d)) v = doubling[d][v];
+    cout << v+1 << endl;
+}
