a3e16a47b16f7abe26e29e3db6071cfe9f646331
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-05-02 09:31:30 +0900

    update Snippet

--- Snippet/source.cpp
+++ Snippet/source.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 
+using namespace atcoder;
 using namespace std;
 #define rep(i,a,b) for(int i = a; i < b; i++)
 #define rrep(i,a,b) for(int i = a; i >= b; i--)
 #define fore(i,a) for(auto& i : a)
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define pcnt __builtin_popcount
 using ll = long long;
 using pint = pair<int, int>;
 using pll = pair<long long, long long>;
+using NGraph = vector<vector<int>>;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 const int IINF = 1000100100;
