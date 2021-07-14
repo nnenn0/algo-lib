6ab1d01f1b10508cee4e00dc7b87e8daa8bc77db
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-07-14 22:58:11 +0900

    add queueを使用したしゃくとり法

---
+++ Other/two_pointers_queue.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/* 
+    two_pointers_queue queueを使用したしゃくとり法
+    O(N)
+    example: ABC032C 列
+    https://atcoder.jp/contests/abc032/tasks/abc032_c
+*/
+
+int main() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < n; ++i) {
+        if (a[i] == 0) {
+            cout << n << endl;
+            return 0;
+        }
+    }
+    int res = 0;
+    long long p = 1;
+    queue<int> q;
+    for (auto ai : a) {
+        q.push(ai);
+        p *= ai;
+        while (q.size() && p > k) {
+            int rm = q.front(); q.pop();
+            p /= rm;
+        }
+        res = max(res, int(q.size()));
+    }
+    cout << res << endl;
+}
