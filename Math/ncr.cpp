528f02e052997ce89ff24ad8428be97ba7236870
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-07-16 22:23:47 +0900

    add オーバーフロー対策

--- Math/ncr.cpp
+++ Math/ncr.cpp
@@ -17,8 +17,19 @@ long long nCr(long long n, long long r) {
     return res;
 }
 
+long long nCr2(long long n, long long r) {
+    long long res = 1;
+    for (int i = 0; i < r; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
+}
+
 int main() {
     using namespace std;
     cout << nCr(6, 6) << endl;
     cout << nCr(4, 2) << endl;
+    cout << nCr2(6, 6) << endl;
+    cout << nCr2(4, 2) << endl;
 }
