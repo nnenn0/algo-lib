7af7b9d37841b2f7ce8b6272ff867f4642470e64
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-07-21 23:11:02 +0900

    add アフィン変換

---
+++ Geometry/affine_transformation.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+    アフィン変換
+    3次元の行列で扱うことで平行移動も可能になる。
+    横にx倍、縦にy倍: {{x, 0, 0}, {0, y, 0}, {0, 0, 1}} {a, b, 1}
+    原点を中心にθ度の回転: {{cosθ, -sinθ, 0}, {sinθ, cosθ, 0}, {0, 0, 1}} {a, b, 1}
+    平行移動: {{1, 0, x}, {0, 1, y}, {0, 0, 1}} {a, b, 1}
+    example: ABC189E Rotate and Flip
+    https://atcoder.jp/contests/abc189/tasks/abc189_e
+*/
+
+using ll = long long;
+using pll = pair<ll, ll>;
+
+struct Matrix {
+    vector<vector<ll>> matrix;
+    Matrix() {
+        matrix = vector<vector<ll>> {
+            {1, 0, 0},
+            {0, 1, 0},
+            {0, 0, 1}
+        };
+    }
+    Matrix(vector<vector<ll>> m) {
+        matrix = m;
+    }
+    vector<vector<ll>> get() {
+        return matrix;
+    }
+    Matrix calc(Matrix a) {
+        return calc(a.get());
+    }
+    Matrix calc(vector<vector<ll>> a) {
+        vector<vector<ll>> cal(3, vector<ll>(3, 0));
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                for (int k = 0; k < 3; ++k) {
+                    cal[i][j] += a[i][k] * matrix[k][j];
+                }
+            }
+        }
+        return Matrix(cal);
+    }
+    Matrix clockwise() {
+        vector<vector<ll>> a {
+            {0, 1, 0},
+            {-1, 0, 0},
+            {0, 0, 1}
+        };
+        return calc(a);
+    }
+    Matrix counter_clockwise() {
+        vector<vector<ll>> a {
+            {0, -1, 0},
+            {1, 0, 0},
+            {0, 0, 1}
+        };
+        return calc(a);
+    }
+    Matrix symmetric_x(ll p) {
+        vector<vector<ll>> a {
+            {-1, 0, 2*p},
+            {0, 1, 0},
+            {0, 0, 1}
+        };
+        return calc(a);
+    }
+    Matrix symmetric_y(ll p) {
+        vector<vector<ll>> a {
+            {1, 0, 0},
+            {0, -1, 2*p},
+            {0, 0, 1}
+        };
+        return calc(a);
+    }
+};
+
+int main() {
+    int N; cin >> N;
+    vector<pll> points(N);
+    for (int i = 0; i < N; ++i)  {
+        ll x, y; cin >> x >> y;
+        points[i] = {x, y};
+    }
+    int M; cin >> M;
+    vector<Matrix> states(M+1);
+    for (int i = 1; i <= M; ++i)  {
+        int op; cin >> op;
+        if (op == 1) states[i] = states[i-1].clockwise();
+        if (op == 2) states[i] = states[i-1].counter_clockwise();
+        if (op == 3) {
+            ll p; cin >> p;
+            states[i] = states[i-1].symmetric_x(p);
+        }
+        if (op == 4) {
+            ll p; cin >> p;
+            states[i] = states[i-1].symmetric_y(p);
+        }
+    }
+    int Q; cin >> Q;
+    for (int _ = 0; _ < Q; ++_)  {
+        int a, b; cin >> a >> b; --b;
+        vector<vector<ll>> m {
+            {points[b].first, 0, 0},
+            {points[b].second, 0, 0},
+            {1, 0, 0}
+        };
+        Matrix matrix(m);
+        auto res = matrix.calc(states[a]).get();
+        cout << res[0][0] << " " << res[1][0] << endl;
+    }
+}
