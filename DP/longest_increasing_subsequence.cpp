d1f5b207d823b559557fdd4e085e92cfc21ae097
nnenn0 <nnenn0memo@gmail.com>
2021-07-22 19:44:30 +0900

    update 同じ値が連続することを許す

--- DP/longest_increasing_subsequence.cpp
+++ DP/longest_increasing_subsequence.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 /*
     LIS O(NlogN)
     最長増加部分列(LIS)を求める
+    is_strong = falseの時同じ値が連続することを許す
     example: Longest Increasing Subsequence
     https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_D&lang=jp
 */
 
 constexpr int INF = 1000100100;
 
-int LIS(const vector<int>& vec) {
+int LIS(const vector<int>& vec, bool is_strong = true) {
     int n = int(vec.size());
     vector<int> dp(n, INF);
     for (auto& v : vec) {
-        *lower_bound(dp.begin(), dp.end(), v) = v;
+        if (is_strong) *lower_bound(dp.begin(), dp.end(), v) = v;
+        else *upper_bound(dp.begin(), dp.end(), v) = v;
     }
     return int(lower_bound(dp.begin(), dp.end(), INF) - dp.begin());
 }
