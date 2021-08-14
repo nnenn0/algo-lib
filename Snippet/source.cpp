a8bd3d69021bf6cf0d496fa5d7c5aba5704b8dfa
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-08-15 01:51:32 +0900

    update main関数

--- Snippet/source.cpp
+++ Snippet/source.cpp
@@ -3,21 +3,25 @@
 
 using namespace atcoder;
 using namespace std;
-#define rep(i,a,b) for(int i = a; i < b; i++)
-#define rrep(i,a,b) for(int i = a; i >= b; i--)
+#define rep(i,a,b) for(long long i = a; i < b; i++)
+#define rrep(i,a,b) for(long long i = a; i >= b; i--)
 #define fore(i,a) for(auto& i : a)
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 #define pcnt __builtin_popcount
+#define loop while(true)
 using ll = long long;
 using pint = pair<int, int>;
 using pll = pair<long long, long long>;
 using NGraph = vector<vector<int>>;
+template<typename T> using rev_priority_queue = priority_queue<T, vector<T>, greater<T>>;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 const int IINF = 1000100100;
 const long long LINF = 1LL << 60;
+template<class T> int former(const vector<T> &v, T x) { return upper_bound(v.begin(), v.end(), x) - v.begin() - 1; }
+template<class T> int latter(const vector<T> &v, T x) { return lower_bound(v.begin(), v.end(), x) - v.begin(); }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
