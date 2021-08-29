18ad1fedab215d8c4ee26c310682fb4f0e627303
nnenn0 <nnenn0memo@gmail.com>
2021-08-29 20:22:55 +0900

    update インデント修正

--- DataStructure/segment_tree.cpp
+++ DataStructure/segment_tree.cpp
@@ -69,8 +69,8 @@ struct SegmentTree {
         if (need_build) assert(0 && "Need Build");
         Monoid L = M1, R = M1;
         for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
-        if(a & 1) L = f(L, seg[a++]);
-        if(b & 1) R = f(seg[--b], R);
+            if(a & 1) L = f(L, seg[a++]);
+            if(b & 1) R = f(seg[--b], R);
         }
         return f(L, R);
     }
@@ -94,17 +94,17 @@ struct SegmentTree {
     int find_first(int a, const C &check) {
         Monoid L = M1;
         if(a <= 0) {
-        if(check(f(L, seg[1]))) return find_subtree(1, check, L, false);
-        return -1;
+            if(check(f(L, seg[1]))) return find_subtree(1, check, L, false);
+            return -1;
         }
         int b = sz;
         for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
-        if(a & 1) {
-            Monoid nxt = f(L, seg[a]);
-            if(check(nxt)) return find_subtree(a, check, L, false);
-            L = nxt;
-            ++a;
-        }
+            if(a & 1) {
+                Monoid nxt = f(L, seg[a]);
+                if(check(nxt)) return find_subtree(a, check, L, false);
+                L = nxt;
+                ++a;
+            }
         }
         return -1;
     }
@@ -113,16 +113,16 @@ struct SegmentTree {
     int find_last(int b, const C &check) {
         Monoid R = M1;
         if(b >= sz) {
-        if(check(f(seg[1], R))) return find_subtree(1, check, R, true);
-        return -1;
+            if(check(f(seg[1], R))) return find_subtree(1, check, R, true);
+            return -1;
         }
         int a = sz;
         for(b += sz; a < b; a >>= 1, b >>= 1) {
-        if(b & 1) {
-            Monoid nxt = f(seg[--b], R);
-            if(check(nxt)) return find_subtree(b, check, R, true);
-            R = nxt;
-        }
+            if(b & 1) {
+                Monoid nxt = f(seg[--b], R);
+                if(check(nxt)) return find_subtree(b, check, R, true);
+                R = nxt;
+            }
         }
         return -1;
     }
